aa2a848f7ddc4d4fe2c60ed407997a1fe7eafe67
Syifa Saif Hidayat <syifa.sh404@students.amikom.ac.id>
2022-08-17 14:52:36 +0700

    constants colors

---
+++ lib/constant/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color kBackground = Color(0xFF242A32);
+const Color kPrimaryColor = Color(0xFF4FCCA3);
+const Color kSecondaryColor = Color(0xFF3A3F47);
