c1784978b3cb02ff916dfadb66f53f63b646aeea
andrisilaban <andriantosilaban@gmail.com>
2022-08-27 11:54:04 +0700

    Add API Link

---
+++ lib/api/api.dart
@@ -0,0 +1,9 @@
+//https://api.themoviedb.org/3/movie/now_playing?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/popular?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/upcoming?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/top_rated?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
+//search movie by id - bookmark
+//https://api.themoviedb.org/3/movie/19404?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US
+//https://api.themoviedb.org/3/genre/movie/list?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US
+//Noted Categories top rated, latest, popular, now_playing
+// klik load more -> page baru menampilkan list movie berdasarkan categories
