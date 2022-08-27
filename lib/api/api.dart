b921c76b8bf73b588c6efa9646a2c504c21d0a11
andrisilaban <andriantosilaban@gmail.com>
2022-08-27 13:04:07 +0700

    Reposition API

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,7 +1,8 @@
+//https://api.themoviedb.org/3/movie/latest?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
 //https://api.themoviedb.org/3/movie/now_playing?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
 //https://api.themoviedb.org/3/movie/popular?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
-//https://api.themoviedb.org/3/movie/upcoming?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
 //https://api.themoviedb.org/3/movie/top_rated?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/upcoming?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US&page=1
 //search movie by id - bookmark
 //https://api.themoviedb.org/3/movie/19404?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US
 //https://api.themoviedb.org/3/genre/movie/list?api_key=ebd2cdfaaa90ae5ce496bc143d03cdc4&language=en-US
